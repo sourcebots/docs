51654f7121e68b24c5f2a3fa43edeabdf4f81c1a
Jake Howard <git@theorangeone.net>
2017-10-12 19:57:17 +0100

    DOnt install rsync, make the build even faster!

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -16,8 +16,8 @@ git config --global user.name $CIRCLE_USERNAME
 git clone -q --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
 
 cd $DEPLOY_DIR
-rsync -ar --delete ../public/* .
-
+rm -rf *
+cp ../public/* . -r
 git add -f .
 git commit -m "Deploy build $CIRCLE_BUILD_NUM [ci skip]" || true
 git push -f
